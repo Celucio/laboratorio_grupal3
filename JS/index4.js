719ca861deaffabafd64e3c86e09db9b0b7b5f84
Celucio <75482093+Celucio@users.noreply.github.com>
2021-12-23 07:42:39 -0500

    o

--- JS/index4.js
+++ JS/index4.js
@@ -1,15 +1,15 @@
 var categoria;
-    var precio;
-    var asientos;
-    var fecha;
-    var calidad;
-    var nombre;
-    
-    nombre=prompt("Escriba el nombre de la pelicula: ", "Nombre");
-    categoria=prompt("Escriba la categoria de la pelicula: ", "Categoria");
-    asientos=parseInt(prompt("Escriba el numero de asientos que desea: ", "Asientos"));
-    precio=5.50;
-    fecha=prompt("Escriba el dia que desea ver la pelicula", "Fecha");
-    calidad=prompt("Escriba la calidad de su pelicula", "Calidad");
-    
-    document.write("PELICULA: " + nombre + "<br>", "CATEGORIA: " + categoria + "<br>", " PRECIO ASIENTOS: " + (asientos*precio) + "<br>", "FECHA: " + fecha + "<br>","CALIDAD: " +calidad );
+var precio;
+var asientos;
+var fecha;
+var calidad;
+var nombre;
+
+nombre = prompt("Escriba el nombre de la pelicula: ", "Nombre");
+categoria = prompt("Escriba la categoria de la pelicula: ", "Categoria");
+asientos = parseInt(prompt("Escriba el numero de asientos que desea: ", "Asientos"));
+precio = 5.50;
+fecha = prompt("Escriba el dia que desea ver la pelicula", "Fecha");
+calidad = prompt("Escriba la calidad de su pelicula", "Calidad");
+
+document.write("PELICULA: " + nombre + "<br>", "CATEGORIA: " + categoria + "<br>", " PRECIO ASIENTOS: " + (asientos * precio) + "<br>", "FECHA: " + fecha + "<br>", "CALIDAD: " + calidad);
